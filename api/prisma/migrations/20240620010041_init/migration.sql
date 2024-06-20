3d37b201df31c1667f5254a4f6c4095216248030
Maxi Bono <maximiliano.bono@usm.cl>
2024-06-19 21:02:22 -0400

    Migracion a PostgreSQL

---
+++ api/prisma/migrations/20240620010041_init/migration.sql
@@ -0,0 +1,56 @@
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" SERIAL NOT NULL,
+    "direccion_correo" TEXT NOT NULL,
+    "descripcion" TEXT,
+    "fechaCreacion" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Correo" (
+    "id" SERIAL NOT NULL,
+    "remitente_id" INTEGER NOT NULL,
+    "destinatario_id" INTEGER NOT NULL,
+    "asunto" TEXT NOT NULL,
+    "cuerpo" TEXT NOT NULL,
+    "fecha_envio" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Correo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Direcciones_bloqueadas" (
+    "id" SERIAL NOT NULL,
+    "usuario_id" INTEGER NOT NULL,
+    "direccion_bloqueada" TEXT NOT NULL,
+    "fecha_bloqueo" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Direcciones_bloqueadas_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Direcciones_favoritas" (
+    "id" SERIAL NOT NULL,
+    "usuario_id" INTEGER NOT NULL,
+    "direccion_favorita" TEXT NOT NULL,
+    "fecha_agregado" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Direcciones_favoritas_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuario_direccion_correo_key" ON "Usuario"("direccion_correo");
+
+-- AddForeignKey
+ALTER TABLE "Correo" ADD CONSTRAINT "Correo_remitente_id_fkey" FOREIGN KEY ("remitente_id") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Correo" ADD CONSTRAINT "Correo_destinatario_id_fkey" FOREIGN KEY ("destinatario_id") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Direcciones_bloqueadas" ADD CONSTRAINT "Direcciones_bloqueadas_usuario_id_fkey" FOREIGN KEY ("usuario_id") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Direcciones_favoritas" ADD CONSTRAINT "Direcciones_favoritas_usuario_id_fkey" FOREIGN KEY ("usuario_id") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
